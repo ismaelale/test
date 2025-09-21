90e070c0df94641b4c8ff4a2d0d9714222e9999f
ismael12 <imuozcabrera12@gmail.com>
2025-09-20 20:46:48 -0600

    No hice nada, solo lo deje ahí para mañana

---
+++ arrayList2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class arrayList2 {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+
+        ArrayList<Integer> numero = new ArrayList<>();
+
+        System.out.println("INGRESE LOS NUMEROS");
+        
+        
+
+    }
+}
